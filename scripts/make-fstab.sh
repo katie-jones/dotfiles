99ff79ca066c9dc845de0ff7f601180d60973dd8
Katie <kjones@epfl.ch>
2016-04-30 12:15:36 +0200

    Update make-fstab

--- scripts/make-fstab.sh
+++ scripts/make-fstab.sh
@@ -3,7 +3,7 @@
 
 FORMAT="%-46s\t%-24s\t%-13s\t%-18s\t%-2s\t%-2s\n" # printf format
 
-ROOT_UUID="d957d314-2629-418d-b1e5-64bc78d03a64"
+ROOT_UUID="00fbd50f-7a98-48e8-a135-3b3ba00461c8"
 SWAP_UUID="01098383-d26f-4fc3-a570-2fa65f767a0c"
 SHARED_UUID="2b404128-4898-37a3-87e4-ea4a12bf1698"
 SCHOOL_UUID="8e91e673-2782-3a44-b24c-a6112035cb4e"
@@ -11,7 +11,9 @@ HOME_UUID="176525c7-1bf1-425d-9745-e4ec860a4b0c"
 MATLAB_UUID="ac479f9d-78d7-4066-b03a-86945c253d12"
 MATLAB_UUID_TWO="e2620e77-ce61-4d93-8daa-4cf41056883d"
 BOOT_UUID="cdfb3a53-d96f-4ea8-98f8-2559d4ae1e88"
-BACKUP_UUID="81b7995d-8d94-4467-a160-f1dcffad7f9a"
+SCHOOL_UUID="8e91e673-2782-3a44-b24c-a6112035cb4e"
+HOME_BACKUP_UUID="3e41cb52-f707-4186-8f68-b1a2da0d8b3f"
+BACKUP_UUID="62d583a4-3b9b-44a1-a5d0-f68fb8aeae38"
 
 UUIDS=( $ROOT_UUID $SWAP_UUID $SHARED_UUID $SCHOOL_UUID $HOME_UUID $MATLAB_UUID $MATLAB_UUID_TWO $BOOT_UUID $BACKUP_UUID )
 MTPTS=( "/" "none" "/mnt/shared" "/home/katie/school" "/mnt/lhome" "/usr/local/MATLAB/R2014b" "/usr/local/MATLAB/R2009b" "/mnt/boot" "/mnt/backups/root" )
@@ -19,7 +21,6 @@ FORMATS=( "ext4" "swap" "hfsplus" "hfsplus" "ext4" "ext4" "ext4" "ext2" "ext4" )
 OPTIONS=( "errors=remount-ro" "sw" "rw,exec,auto" "rw,exec,auto" "rw,exec,auto" "rw,exec,auto" "rw,exec,auto" "rw,noauto" "rw,noauto" )
 CHECKS=( "0 1" "0 0" "0 2" "0 2" "0 2" "0 2" "0 2" "0 0" "0 0" )
 COMMENTS=( "root partition" "swap" "shared partition" "school partition" "linux home partition" "matlab 2014b partition" "matlab 2009b partition" "arch boot partition" "root backup partition" )
-
 
 n=9
 
