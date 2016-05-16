f684e6ccedc1d66b559da19423029569cfa1c5fc
Katie <kjo@live.ca>
2016-05-16 11:48:30 +0200

    Update fstab values

--- scripts/make-fstab.sh
+++ scripts/make-fstab.sh
@@ -5,23 +5,27 @@ FORMAT="%-46s\t%-24s\t%-13s\t%-18s\t%-2s\t%-2s\n" # printf format
 
 ROOT_UUID="d957d314-2629-418d-b1e5-64bc78d03a64"
 SWAP_UUID="01098383-d26f-4fc3-a570-2fa65f767a0c"
-SHARED_UUID="2b404128-4898-37a3-87e4-ea4a12bf1698"
-SCHOOL_UUID="8e91e673-2782-3a44-b24c-a6112035cb4e"
 HOME_UUID="176525c7-1bf1-425d-9745-e4ec860a4b0c"
+SCHOOL_UUID="012013ff-ff66-4904-92e7-7a81a30a5a2a"
+MEDIA_UUID="500C-AFE1"
+CLOUD_UUID="5b9cbf51-b55c-4a14-8653-2e0153a8f865"
+SHARED_UUID="47a4ee95-9221-4e37-850f-1c515365a3a9"
 MATLAB_UUID="ac479f9d-78d7-4066-b03a-86945c253d12"
 MATLAB_UUID_TWO="e2620e77-ce61-4d93-8daa-4cf41056883d"
 BOOT_UUID="cdfb3a53-d96f-4ea8-98f8-2559d4ae1e88"
 BACKUP_UUID="81b7995d-8d94-4467-a160-f1dcffad7f9a"
-
-UUIDS=( $ROOT_UUID $SWAP_UUID $SHARED_UUID $SCHOOL_UUID $HOME_UUID $MATLAB_UUID $MATLAB_UUID_TWO $BOOT_UUID $BACKUP_UUID )
-MTPTS=( "/" "none" "/mnt/shared" "/home/katie/school" "/mnt/lhome" "/usr/local/MATLAB/R2014b" "/usr/local/MATLAB/R2009b" "/mnt/boot" "/mnt/backups/root" )
-FORMATS=( "ext4" "swap" "hfsplus" "hfsplus" "ext4" "ext4" "ext4" "ext2" "ext4" )
-OPTIONS=( "errors=remount-ro" "sw" "rw,exec,auto" "rw,exec,auto" "rw,exec,auto" "rw,exec,auto" "rw,exec,auto" "rw,noauto" "rw,noauto" )
-CHECKS=( "0 1" "0 0" "0 2" "0 2" "0 2" "0 2" "0 2" "0 0" "0 0" )
-COMMENTS=( "root partition" "swap" "shared partition" "school partition" "linux home partition" "matlab 2014b partition" "matlab 2009b partition" "arch boot partition" "root backup partition" )
+BACKUP_HOME_UUID="3e41cb52-f707-4186-8f68-b1a2da0d8b3f"
+BACKUP_SHARED_UUID="ec4c86d1-091e-359c-b798-11b227a5650f"
 
 
-n=9
+UUIDS=( $ROOT_UUID $SWAP_UUID $HOME_UUID $SCHOOL_UUID $MEDIA_UUID $CLOUD_UUID $SHARED_UUID $MATLAB_UUID $MATLAB_UUID_TWO $BOOT_UUID $BACKUP_UUID $BACKUP_HOME_UUID $BACKUP_SHARED_UUID )
+MTPTS=( "/" "none" "/mnt/lhome" "/home/katie/school" "/mnt/media" "/mnt/cloud" "/mnt/shared" "/usr/local/MATLAB/R2014b" "/usr/local/MATLAB/R2009b" "/mnt/boot" "/mnt/backups/root" "/mnt/backups/home" "/mnt/backups/shared" )
+FORMATS=( "ext4" "swap" "ext4" "ext4" "exfat" "ext4" "ext4" "ext4" "ext4" "ext2" "ext4" "hfsplus" "ext4" "hfsplus" )
+OPTIONS=( "errors=remount-ro" "sw" "rw,exec,auto" "rw,exec,auto" "rw,exec,auto" "rw,exec,auto" "rw,exec,auto" "rw,exec,auto" "rw,exec,auto" "rw,noauto" "rw,noauto" "rw,user,exec,auto" "rw,noauto" "rw,noauto" )
+CHECKS=( "0 1" "0 0" "0 2" "0 2" "0 2" "0 2" "0 2" "0 2" "0 2" "0 0" "0 0" "0 2" "0 0" "0 0" )
+COMMENTS=( "root partition" "swap" "linux home partition" "school partition" "media partition" "cloud partition" "shared partition" "matlab 2014b partition" "matlab 2009b partition" "arch boot partition" "root backup partition" "school partition" "home backup partition" "shared backup partition" )
+
+n=13
 
 printf "# Begin /etc/fstab\n\n"
 
