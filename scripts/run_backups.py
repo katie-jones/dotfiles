79ed2b59dee7fda79a2baebdfd545881887a9e68
Katie <kjones@epfl.ch>
2017-02-19 20:39:02 +0100

    Update backups script

--- scripts/run_backups.py
+++ scripts/run_backups.py
@@ -1,21 +1,5 @@
 #!/usr/bin/python3
 
-# Script to make backups of root and home directories
-# Input: "home" and/or "full" and/or "shared" and/or "media"
-# Options:
-#   -f: Run backup without checking if the last backup is outdated
-#
-# Examples:
-# sudo ./run_backups home full # update home and full backups
-# sudo ./run_backups -f full # make full backups without checking timestamp
-#
-# If script is run from a shell output and error will be sent to the shell.
-# Otherwise (i.e. cron job) output and error will be sent to the $logfile and
-# $errfile respectively.
-#
-# Customizations (folder locations etc.) should all be in the functions
-# set_home_vars, set_full_vars and set_common_vars
-
 import sys
 import os
 import traceback
@@ -31,9 +15,15 @@ class BackupManager:
 
     def __init__(self, args):
         self.args = args
+
         # Parse config from config file
-        self.global_config = self.parse_config(
-            self.args.config_filename, self.SYSTEM_CONFIG_FILENAME)
+        files_to_parse = []
+        if len(self.args.config_filename) > 0:
+            files_to_parse.append(self.args.config_filename)
+
+        files_to_parse.append(self.SYSTEM_CONFIG_FILENAME)
+
+        self.global_config = self.parse_config(files_to_parse)
 
         # Make log and history dirs
         try:
@@ -43,7 +33,7 @@ class BackupManager:
 
     def run(self):
         history_writer = configparser.ConfigParser()
-    
+
         # Loop through sections and run backups for each one
         for section in self.global_config.sections():
             config_section = self.global_config[section]
@@ -63,25 +53,31 @@ class BackupManager:
             self.logfile = open(config_section.get('logfile'), 'w')
             self.errfile = open(config_section.get('errfile'), 'w')
 
+            # Log which section we're running
+            self.log('Date: {:s}'.format(
+                self.get_current_time_formatted('%Y-%m-%d')), False)
+            self.log('Running section {:s}'.format(section))
+
             # Run backups
             if self.run_backups(config_section, self.args):
                 # If backups were successful, add an entry in the cache file
                 history_writer.read_dict({section: {'last_backup':
                                                     int(time.time())}})
+            # Close log and error files
             self.logfile.close()
             self.errfile.close()
-    
+
         with open(self.HISTORY_FILENAME, 'w') as history_file:
             history_writer.write(history_file)
 
     @staticmethod
-    def parse_config(config_filename, system_config_filename = ''):
+    def parse_config(files_to_parse):
         '''
         Parse config from a config file.
         '''
         parser = configparser.ConfigParser()
         parser.read_dict(BackupManager.get_default_config())
-        parser.read([config_filename, system_config_filename])
+        parser.read(files_to_parse)
         return parser
 
     @staticmethod
@@ -101,7 +97,7 @@ class BackupManager:
                              "'frequency_seconds') was given as '{:s}', "
                              "which is not an int!".format(config.name,
                                  config.get('frequency_seconds')))
-    
+
         # Set all directory arguments to have no trailing slash
         directory_args = ['mount_point', 'backup_folder', 'to_backup', 'link_name']
         for arg in directory_args:
@@ -109,7 +105,6 @@ class BackupManager:
                 config[arg] = config[arg][:-1]
 
         return config
-
 
     @staticmethod
     def get_default_config():
@@ -133,27 +128,38 @@ class BackupManager:
                             };
         return config
 
-    def log(self, string):
+    def log(self, string, print_time = True):
         '''
         Log string to logfile.
         '''
+        if print_time:
+            self.logfile.write('[{:s}] '.format(
+                self.get_current_time_formatted()))
+
         self.logfile.write(string + '\n')
         print(string)
 
-    def errlog(self, string):
+    def errlog(self, string, print_time = True):
         '''
         Log string to errfile.
         '''
+        if print_time:
+            self.logfile.write('[{:s}] '.format(self.get_current_time_formatted()))
+
         self.errfile.write(string + '\n')
         print(string)
-    
+
+    @staticmethod
+    def get_current_time_formatted(date_format = '%H:%M:%S'):
+        return datetime.datetime.now().strftime(date_format)
+
     def run_backups(self, section, args):
         '''
         Run a backup based on a section of the config file corresponding to a
         backup of a single directory.
         '''
-    
-        # Run only if backup is outdate (needs to be run again) or if the force 
+
+        # Run only if backup is outdate (needs to be run again) or if the force
         # option was given.
         if args.force_backup or self.backup_outdated(section):
             self.log('Making new backups.')
@@ -164,7 +170,7 @@ class BackupManager:
                 self.errlog('Error: ' + str(e))
                 self.errlog('Exiting.')
                 return
-    
+
             # Make the backups
             try:
                 self.make_backups(section)
@@ -181,9 +187,9 @@ class BackupManager:
                 backups_made = False
         else:
             backups_made = False
-    
+
         return backups_made
-    
+
     def backup_outdated(self, section_config):
         '''
         Return True if the last backup of the directory given in section_config is
@@ -193,45 +199,45 @@ class BackupManager:
         # Read history from cache
         parser = configparser.ConfigParser()
         parser.read('/var/cache/ink/history')
-    
+
         # Check if there is an entry for the given section
         try:
             section_history = parser[section_config.name]
-    
+
             # Read epoch time of last backup
             last_backup = section_history.getint('last_backup', 0)
         except KeyError:
             last_backup = 0
-    
+
         # Check if the last backup is too old
         return (int(time.time()) - last_backup) > \
            section_config.getint('frequency_seconds')
-    
+
     def make_backups(self, section):
         # Get name of new backup folder
-        new_backup_folder_base = os.path.join(section.get('backup_folder'), 
+        new_backup_folder_base = os.path.join(section.get('backup_folder'),
                                    section.get('folder_prefix') + \
                 datetime.datetime.now().strftime('%Y-%m-%dT%H:%M'))
-    
+
         if section.get('to_backup')[0] == '/':
             self.log(section.get('to_backup')[1:])
-            new_backup_folder = os.path.join(new_backup_folder_base, 
+            new_backup_folder = os.path.join(new_backup_folder_base,
                                              section.get('to_backup')[1:])
         else:
-            new_backup_folder = os.path.join(new_backup_folder_base, 
+            new_backup_folder = os.path.join(new_backup_folder_base,
                                              section.get('to_backup'))
-    
+
         self.log('New backup folder: ' + new_backup_folder)
-    
+
         # Make sure new backup folder has a trailing slash
         if new_backup_folder[-1] != '/':
             new_backup_folder = new_backup_folder + '/'
-    
-    
+
+
         # Get name of previous (symlinked) backup folder
         symlink_latest_backup_folder = os.path.join(section.get('backup_folder'),
                                       section.get('link_name'))
-    
+
         # Make directory to hold new backup
         n = 0
         while (1):
@@ -246,53 +252,56 @@ class BackupManager:
             except FileExistsError:
                 n = n + 1
                 continue
-    
+
         # Set up rsync command
-        shell_command = ['sudo', 'rsync', '-a', '--log-file',
+        shell_command = ['sudo', 'rsync', '-ax', '--log-file',
                          section.get('logfile')]
-    
+
         # Check if symlink to previous backup exists
         if os.path.isdir(symlink_latest_backup_folder):
             shell_command.append('--link-dest=' + symlink_latest_backup_folder)
-    
+
         # Check if exclude file given
-        if len(section.get('exclude_file')) > 0:
+        if len(section.get('exclude_file')) > 0 and \
+           os.path.exists(section.get('exclude_file')):
             shell_command.append('--exclude-from=' + section.get('exclude_file'))
-    
+
         # Add source and destination
-        shell_command.extend([section.get('to_backup') + '/', new_backup_folder])
-    
+        if section.get('to_backup') == '/':
+            shell_command.extend([section.get('to_backup'), new_backup_folder])
+        else:
+            shell_command.extend([section.get('to_backup') + '/', new_backup_folder])
+
         # Run shell command
         self.log('Running shell command: ' + ' '.join(shell_command))
         p = subprocess.Popen(' '.join(shell_command), shell=True)
         p.communicate()
-    
+
         # Check return code and raise error if command did not succeed
         if p.returncode != 0:
             raise RuntimeError('Making backups with rsync failed.')
-    
+
         # Replace previous symlink
         try:
             os.unlink(symlink_latest_backup_folder)
         except FileNotFoundError:
             pass
         os.symlink(new_backup_folder_base, symlink_latest_backup_folder)
-            
+
         self.log('Backups succeeded.')
-    
-    
+
     def mount_drive(self, section):
         '''
         Mount the partition where the backups should be made.
         '''
         self.log('Checking if partition is already mounted...')
-    
+
         if not self.is_mounted(section.get('mount_point')):
             self.log('Partition not already mounted. Trying to mount it.')
-    
+
             # Initialize basic command
             shell_command = ["sudo", "mount"]
-    
+
             # Check UUID, label and device in that order
             if len(section.get('UUID')) > 0:
                 shell_command.extend(['-U', section.get('UUID')])
@@ -300,23 +309,22 @@ class BackupManager:
                 shell_command.extend(['-L', section.get('partition_label')])
             elif len(section.get('partition_device')) > 0:
                 shell_command.append(section.get('partition_device'))
-    
+
             # Add mount point of partition to shell arguments
             shell_command.append(section.get('mount_point'))
-    
+
             # Mount device
             p = subprocess.Popen(' '.join(shell_command), shell=True)
             p.communicate()
-    
+
             # Check return code and raise error if command did not succeed
             if p.returncode != 0:
                 raise RuntimeError('Mounting backup disk failed.')
-    
+
             self.log('Partition mounted.')
         else:
             self.log('Partition already mounted.')
-    
-    
+
     @staticmethod
     def is_mounted(desired_mount_point):
         partition_mounted = False
@@ -326,41 +334,24 @@ class BackupManager:
                 if mount_point == desired_mount_point:
                     partition_mounted = True
                     break
-    
+
         return partition_mounted
 
 def main():
     args = parse_args(sys.argv[1:])
     backup_manager = BackupManager(args)
     backup_manager.run()
-    #  config = parse_config(args.config_filename)
 
-    #  history_writer = configparser.ConfigParser()
-
-    #  # Loop through sections and run backups for each one
-    #  for section in config.sections():
-        #  config_section = config[section]
-        #  check_config(config_section)
-        #  if run_backups(config_section, args):
-            #  # If backups were successful, add an entry in the cache file
-            #  history_writer.read_dict({section: {'last_backup':
-                                                #  int(time.time())}})
-
-    #  with open('/var/cache/ink/history', 'w') as history_file:
-        #  history_writer.write(history_file)
 def parse_args(argv):
     '''
     Parse command line arguments.
     '''
     parser = argparse.ArgumentParser(description='Make local backups of disk.')
-    parser.add_argument('config_filename', help='Path to configuration file.')
+    parser.add_argument('config_filename', default = '', nargs = '?',
+                        help='Path to configuration file.')
     parser.add_argument('-f', dest='force_backup', action='store_true',
                         help='Force backup regardless of time stamp')
-    parser.add_argument('--system-config', dest='system_config_filename',
-                        default='/etc/ink/inkrc', help='Path to system-wide '
-                        'configuration file.')
     return parser.parse_args(argv)
-    
 
 if __name__ == "__main__":
     main()
