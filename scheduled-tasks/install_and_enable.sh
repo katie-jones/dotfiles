25d0a84687873fd8654657b3831d4f97eeed802a
Katie <kjones@epfl.ch>
2017-02-25 11:04:54 +0100

    Use symlinks for systemd timers and services

--- scheduled-tasks/install_and_enable.sh
+++ scheduled-tasks/install_and_enable.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Script to install systemd timers and enable them
 systemd_dir=/etc/systemd/system/
+orig_dir=/home/katie/linux-config/scheduled-tasks/
 services=("root-daily" "root-hourly" "katie-daily" "katie-hourly")
 
 for unit in "${services[@]}"; do
     echo "Installing $unit..."
-    sudo cp -v "$unit.service" "$unit.timer" $systemd_dir
+    sudo ln -sv "$orig_dir$unit.service" "$orig_dir$unit.timer" $systemd_dir
     echo "Enabling $unit..."
     sudo systemctl enable "$unit.timer"
 done
-# sudo cp root-daily.service root-daily.timer
