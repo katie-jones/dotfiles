3e2e69d82409c2e22a06834d6b16a1a9709e1309
Katie <kjones@epfl.ch>
2017-02-28 08:01:54 +0100

    Run root scripts from /usr/local for better security

--- scheduled-tasks/install_and_enable.sh
+++ scheduled-tasks/install_and_enable.sh
@@ -2,11 +2,18 @@
 # Script to install systemd timers and enable them
 systemd_dir=/etc/systemd/system/
 orig_dir=/home/katie/linux-config/scheduled-tasks/
+root_script_dir=/usr/local/bin
 services=("root-daily" "root-hourly" "katie-daily" "katie-hourly")
+root_scripts=("root-daily" "root-hourly")
 
 for unit in "${services[@]}"; do
     echo "Installing $unit..."
-    sudo ln -sv "$orig_dir$unit.service" "$orig_dir$unit.timer" $systemd_dir
+    sudo cp -v "$orig_dir$unit.service" "$orig_dir$unit.timer" "$systemd_dir"
     echo "Enabling $unit..."
     sudo systemctl enable "$unit.timer"
+done
+
+for script in "${root_scripts[@]}"; do
+    echo "Installing $script..."
+    sudo cp -v "$orig_dir$script.sh" "$root_script_dir"
 done
